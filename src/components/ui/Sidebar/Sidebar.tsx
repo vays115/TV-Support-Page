557469828f699b6c284b4b568a1fc5f2a0160b6b
vay <vsu@connectedliving.com>
2024-12-20 15:14:32 -0500

    click support guide to get to /main

--- src/components/ui/Sidebar/Sidebar.tsx
+++ src/components/ui/Sidebar/Sidebar.tsx
@@ -1,5 +1,7 @@
 import './Sidebar.css';
 
+import Link from 'next/link';
+
 interface SidebarProps {
   children: React.ReactNode;
   title: string;
@@ -9,7 +11,9 @@ const Sidebar = ({ children, title }: SidebarProps) => {
   return (
     <div className="sidebar">
       <div className="sidebar__header">
-        <h1>{title}</h1>
+        <Link href="/main">
+          <h1 className="cursor-pointer hover:opacity-80 transition-opacity">{title}</h1>
+        </Link>
       </div>
       <nav className="sidebar__nav">
         {children}
